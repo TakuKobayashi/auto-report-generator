80016c270442fea0885b7fc66fa9d687e794890a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-04-05 10:56:22 +0900

    とりあえずGithubの情報から日報っぽいmarkdownファイルをつくれるようにした

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 import { program } from 'commander';
 import packageJson from '../package.json';
 import { Github } from './imports/git/github';
+import fs from 'fs';
 
 /**
  * Set global CLI configurations
@@ -22,18 +23,32 @@ program
   .action(async (options: any) => {
     const authtoken = options.authtoken ? options.authtoken : '';
     const github = new Github(authtoken);
+    const githubUser = await github.loadSelfUser();
     const perPage = 100;
     const nowDate = new Date();
     const commits = await github.searchSelfCommits(nowDate, 1, perPage);
     const importSources = commits.items;
-    const totalPage = Math.ceil(commits.total_count / perPage)
-    for(let i = 1;i < totalPage;++i) {
+    const totalPage = Math.ceil(commits.total_count / perPage);
+    for (let i = 1; i < totalPage; ++i) {
       const nextPageCommits = await github.searchSelfCommits(nowDate, i, perPage);
-      for(const item of nextPageCommits.items) {
-        importSources.push(item)
+      for (const item of nextPageCommits.items) {
+        importSources.push(item);
       }
     }
-    console.log(JSON.stringify(importSources, null, 2));
+    const exportLines: string[] = [];
+    exportLines.push(`# ${githubUser.login} ${nowDate.toLocaleDateString('ja-JP')}の日報`);
+    exportLines.push('##今日やったこと');
+    exportLines.push('');
+    exportLines.push(`**本日のコミット件数: ${commits.total_count}**`);
+    exportLines.push(`以下コミットした内容`);
+    for (const importSource of importSources) {
+      const comitterDate = new Date(importSource.commit.committer.date);
+      exportLines.push(
+        `* ${comitterDate.getHours()}:${comitterDate.getMinutes()}:${comitterDate.getSeconds()} [${importSource.repository.full_name}](${importSource.repository.html_url}) ${importSource.commit.message}`,
+      );
+      exportLines.push(`  * 参照先:[${importSource.sha}](${importSource.html_url})`);
+    }
+    fs.writeFileSync(`${nowDate.toLocaleDateString('sv-SE')}-${nowDate.toLocaleTimeString('sv-SE')}-report.md`, exportLines.join('\n'));
   });
 
 program.parse(process.argv);
